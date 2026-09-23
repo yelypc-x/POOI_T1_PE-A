a6860a066886d65b02b3a0b3b7328064b89e49a4
Nayely Peña <yelypenacampos@gmail.com>
2026-09-22 23:38:30 -0500

    Se añadio indez y agregar

--- POOI_T1_PEÑA/Controllers/EmpleadoController.cs
+++ POOI_T1_PEÑA/Controllers/EmpleadoController.cs
@@ -38,5 +38,47 @@ namespace POOI_T1_PEÑA.Controllers
         {
             return View(admin);
         }
+
+
+
+        public static List<Empleado> Planilla = new List<Empleado>
+        {
+            new Empleado { idEmpleado = "001", nomapeEmpleado = "Fernando Rios", categoriaEmpleado = "E1", nHijos = 2, tipoContrato = "Indefinido" },
+            new Empleado { idEmpleado = "002", nomapeEmpleado = "Ana Chavarria", categoriaEmpleado = "E2", nHijos = 1, tipoContrato = "Contratado" },
+            new Empleado { idEmpleado = "003", nomapeEmpleado = "Carlos Campos", categoriaEmpleado = "E3", nHijos = 0, tipoContrato = "Indefinido" },
+            new Empleado { idEmpleado = "004", nomapeEmpleado = "Lupe Vara", categoriaEmpleado = "E1", nHijos = 4, tipoContrato = "Contratado" }
+        };
+
+        // GET
+        public ActionResult Index()
+        {
+            return View(Planilla);
+        }
+
+        // GET
+        public ActionResult Agregar()
+        {
+            Empleado emp = new Empleado();
+            return View(emp);
+        }
+
+        // POST:
+        [HttpPost]
+        public ActionResult Agregar(Empleado emp)
+        {
+            bool existe = Planilla.Any(e => e.idEmpleado == emp.idEmpleado);
+
+            if (existe)
+            {
+                ViewBag.Mensaje = "Error: el ID de Empleado ya existe.";
+            }
+            else
+            {
+                Planilla.Add(emp);
+                ViewBag.Mensaje = "Empleado guardado correctamente.";
+            }
+
+            return View(emp);
+        }
     }
 }
